eebd856d6172aeb0e30e60ed9af18e245f4fdbc6
CactusPuppy <puppycactus@gmail.com>
2021-11-23 12:50:32 -0800

    feat: Add two address blocks for Shanghai HQ
    Also remove Google Maps embed because it didn't do anything

--- pages/index.js
+++ pages/index.js
@@ -144,12 +144,17 @@ export default function Home() {
             <span id='contact' className='anchor'/>
             <h1 className='text-center text-4xl mb-2 font-semibold'>Contact</h1>
             <br/>
-            <div className='flex flex-col sm:flex-row content-evenly justify-center'>
+            <div className='flex flex-col sm:flex-row content-evenly justify-center gap-8'>
                 {/* <iframe className="w-96 h-48" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1669.5229423824994!2d-122.26270389781278!3d37.51492603078649!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f98aa79bc4f55%3A0xd47219d8b27ce42a!2sAnwita%20Biosciences!5e0!3m2!1sen!2sus!4v1632246025164!5m2!1sen!2sus" style={{width: "35rem", height: "30rem", maxWidth: "95%", maxHeight: "35rem", border: 0}} allow="fullscreen" allowFullScreen={true} loading="lazy"></iframe> */}
-              <div className='px-0 sm:px-8'>
+              <div className='sm:text-right'>
                 <p>Company Address: <br/><a href='https://www.google.com/maps/place/300+Industrial+Rd,+San+Carlos,+CA+94070/@37.5149181,-122.2641003,17z/data=!3m1!4b1!4m5!3m4!1s0x808f98aa79063b39:0x9744f7cb0ddf62fa!8m2!3d37.5149181!4d-122.2619063' target="_blank" rel="noreferrer noopener" className={linkStyling}>300 Industrial Road,<br/>San Carlos, CA 94070</a></p>
-                <p>Phone: <a href='tel:1-650-600-9828' target='_blank' className={linkStyling}>(650) 600-9828</a></p>
-                <p>Email: <a href='mailto:contact@anwitabio.com' className={linkStyling}>contact@anwitabio.com</a></p>
+                <p>Phone:&nbsp;<a href='tel:1-650-600-9828' target='_blank' className={linkStyling}>(650) 600-9828</a></p>
+                <p>Email:&nbsp;<a href='mailto:contact@anwitabio.com' className={linkStyling}>contact@anwitabio.com</a></p>
+              </div>
+              <div>
+                <p>Company Address: <br/><a href='https://www.google.com/maps/place/300+Industrial+Rd,+San+Carlos,+CA+94070/@37.5149181,-122.2641003,17z/data=!3m1!4b1!4m5!3m4!1s0x808f98aa79063b39:0x9744f7cb0ddf62fa!8m2!3d37.5149181!4d-122.2619063' target="_blank" rel="noreferrer noopener" className={linkStyling}>300 Industrial Road,<br/>San Carlos, CA 94070</a></p>
+                <p>Phone:&nbsp;<a href='tel:1-650-600-9828' target='_blank' className={linkStyling}>(650) 600-9828</a></p>
+                <p>Email:&nbsp;<a href='mailto:contact@anwitabio.com' className={linkStyling}>contact@anwitabio.com</a></p>
               </div>
             </div>
 
