30a8bc0b99d0eb9213d57c2ba30f73af5c4dc481
CactusPuppy <puppycactus@gmail.com>
2021-12-10 11:41:08 -0800

    chore: Remove unnecessary import

--- components/Pipeline.js
+++ components/Pipeline.js
@@ -3,7 +3,6 @@ import React from "react";
 import he from "he";
 import ReactMarkdown from "react-markdown";
 import remarkSuperSub from "remark-supersub";
-import Image from "next/image";
 
 function Program(props) {
   const pipeline = props.pipeline;
