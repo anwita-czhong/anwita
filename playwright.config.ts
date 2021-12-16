1bce36a4bebd3233b614c513358a165b4fadc7a6
CactusPuppy <puppycactus@gmail.com>
2021-12-16 14:27:32 -0800

    test(Playwright): Don't automatically open HTML report

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ const config: PlaywrightTestConfig = {
   workers: process.env.CI ? 1 : 5,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: process.env.CI ? "dot" : [["html", { open: "never" }]],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
