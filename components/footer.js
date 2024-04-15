47e62a98a314034a07f4dfe5f401d13409b6af44
Cactus Puppy <puppycactus@gmail.com>
2024-04-14 21:27:00 -0700

    build(Yarn): bump Next.js to v13

--- components/footer.js
+++ components/footer.js
@@ -12,20 +12,20 @@ export const Footer = () => {
           <h1 className='text-white text-2xl font-bold'>About</h1>
           <hr className='my-3 h-px border-1 border-gray-400'></hr>
           <p className='text-white font-light'>
-            <Link href='#about'><a className={linkStyling}>Anwita Biosciences</a></Link> is a biopharmaceutical company headquartered in the San Francisco Bay Area.</p>
+            <Link href='#about' className={linkStyling}>Anwita Biosciences</Link> is a biopharmaceutical company headquartered in the San Francisco Bay Area.</p>
         </div>
         <div className={footerItemContainerClasses}>
           <h1 className='text-white text-2xl font-bold'>Careers</h1>
           <hr className='my-3 h-px border-1 border-gray-400'></hr>
-          <p className='text-white font-light'>Visit our <Link href='#careers'><a className={linkStyling}>careers</a></Link> page for listings.</p>
+          <p className='text-white font-light'>Visit our <Link href='#careers' className={linkStyling}>careers</Link> page for listings.</p>
         </div>
         <div className={footerItemContainerClasses}>
           <h1 className='text-white text-2xl font-bold'>Contact Us</h1>
           <hr className='my-3 h-px border-1 border-gray-400'></hr>
           <p className='text-white font-light'>
-                        Phone: <Link href='tel:1-650-600-9828'><a className={linkStyling}>(650) 600-9828</a></Link>
+                        Phone: <Link href='tel:1-650-600-9828' className={linkStyling}>(650) 600-9828</Link>
             <br/>
-                        Email: <Link href='mailto:contact@anwitabio.com'><a className={linkStyling}>contact@anwitabio.com</a></Link>
+                        Email: <Link href='mailto:contact@anwitabio.com' className={linkStyling}>contact@anwitabio.com</Link>
           </p>
         </div>
         <div className={footerItemContainerClasses}>
