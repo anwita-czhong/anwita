22162bc204b97e9474f396a031facfc593ec56e9
Cactus Puppy <puppycactus@gmail.com>
2024-04-15 18:07:39 -0700

    test(Playwright): fix Playwright test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,15 +1,14 @@
-import test from "./support/next-fixture";
-import { expect } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
-test("main page title", async ({ page, port }) => {
-  await page.goto(`http://localhost:${port}/`);
+test("main page title", async ({ page }) => {
+  await page.goto("/");
 
   const name = await page.locator("h1").allInnerTexts();
   expect(name).toContain("Anwita Biosciences");
 });
 
-test("can find the current mission statement", async ({ page, port }) => {
-  await page.goto(`http://localhost:${port}`);
+test("can find the current mission statement", async ({ page }) => {
+  await page.goto("/");
 
   const aboutSection = await page.innerText("div:has(span#about)");
   expect(aboutSection.includes("Our mission is to deliver transformative treatment")).toBeTruthy();
